ea71091c90924bb36d2bb4cd5dbbde682342c1c1
Thashitharan Rajaratnam <thashitharan.rajaratnam@ifs.com>
2021-05-16 23:15:07 +0530

    React project initialized

---
+++ ipl-dashboard/src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { TeamApp } from './pages/TeamPage';
+
+function App() {
+  return (
+    <div className="App">
+      <TeamApp />
+    </div>
+  );
+}
+
+export default App;
